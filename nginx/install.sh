fa41ec90de6b569dd89cc6e80417c6e995ffe26b
skalsin <sergeykalsin@gmail.com>
2021-02-09 14:23:17 +0000

    [nginx] generate self signed certificates for nginx

--- nginx/install.sh
+++ nginx/install.sh
@@ -3,6 +3,12 @@
 set -x 
 set -e
 
+if [ ! -d /etc/nginx/ssl ] ; then
+  mkdir /etc/nginx/ssl
+  ./gen_cert.sh
+  mv cert.pem key.pem /etc/nginx/ssl
+fi
+
 cp sites-available/* /etc/nginx/sites-available/
 cp conf.d/* /etc/nginx/conf.d/
 if [ -f /etc/nginx/nginx.conf -a  ! -f /etc/nginx/nginx.conf.back ] ; then
