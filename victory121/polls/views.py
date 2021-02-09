c122205493271e60bfc8f80d6cc32904c397aa95
dkalsin <dmitriy.gpro@gmail.com>
2021-02-09 10:57:32 +0000

    Создал 2 странчки polls & contact. Разместил по директориям и сделал ссылки url запросов с главной страницы

--- victory121/polls/views.py
+++ victory121/polls/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+#	print(dir(request))
+	return HttpResponse("Hello,world. You're at the polls index.")
+
+def test(request):
+	return  HttpResponse('<h1>Тестовая страница.<h1/>')
