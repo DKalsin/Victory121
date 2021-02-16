b65982132b6e2910edb3d1df563d38b7edcf377a
skalsin <sergeykalsin@gmail.com>
2021-02-16 13:07:05 +0000

    rename gen_cert, add kibana.conf, removed developer.conf

--- nginx/gen_cert.sh
+++ nginx/gen_cert.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-IP=$(hostname -I | egrep -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | head -n1)
+# IP=$(hostname -I | egrep -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | head -n1)
+IP=157.230.119.208
 
-openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout key.pem -out cert.pem -config <(echo "[req]
+echo > conf "[req]
 default_bits  = 2048
 distinguished_name = req_distinguished_name
 req_extensions = req_ext
@@ -25,4 +26,8 @@ subjectAltName = @alt_names
 [alt_names]
 IP.1 = $IP
 DNS.1 = victory121.kz
-")
+"
+openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout key.pem -out cert.pem -config  conf
+rm conf
+mkdir -p /etc/nginx/ssl/
+mv -f cert.pem key.pem /etc/nginx/ssl/
